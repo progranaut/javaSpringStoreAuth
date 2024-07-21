43095449fa7d3628ab30b773b6243c0285529975
progranaut <progranaut@gmail.com>
2024-07-21 12:34:26 +0300

    fix

--- src/main/java/com/ivlev/JavaSpringStore/security/repository/RefreshTokenRepository.java
+++ src/main/java/com/ivlev/JavaSpringStore/security/repository/RefreshTokenRepository.java
@@ -2,11 +2,13 @@ package com.ivlev.JavaSpringStore.security.repository;
 
 import com.ivlev.JavaSpringStore.security.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Ref;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, UUID> {
 
     Optional<RefreshToken> findByToken(String token);
--- src/main/java/com/ivlev/JavaSpringStore/security/repository/SecurityUserRepository.java
+++ src/main/java/com/ivlev/JavaSpringStore/security/repository/SecurityUserRepository.java
@@ -2,10 +2,12 @@ package com.ivlev.JavaSpringStore.security.repository;
 
 import com.ivlev.JavaSpringStore.security.entity.SecurityUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface SecurityUserRepository extends JpaRepository<SecurityUser, UUID> {
 
     Optional<SecurityUser> findByName(String name);
